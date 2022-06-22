b44c5854000de9e989dbd0d333f17919009f87d7
Reece Dunham <me@rdil.rocks>
2022-06-22 13:13:22 -0400

    Revert "Remove $s from inner class names to trick quiltflower into being more correct"
    This reverts commit b51d995270dba3bc6ef34bbb461ecc3c80e42972.

--- src/main/java/alaphant/mixin/auth/MixinAuthorizationUtils_GenericAuthorization.java
+++ src/main/java/alaphant/mixin/auth/MixinAuthorizationUtils_GenericAuthorization.java
@@ -2,7 +2,7 @@ package alaphant.mixin.auth;
 
 import alaphant.util.JwtUtil;
 import com.auth0.jwt.interfaces.DecodedJWT;
-import com.xk72.charles.lib.AuthorizationUtils_GenericAuthorization;
+import com.xk72.charles.lib.AuthorizationUtils$GenericAuthorization;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -10,8 +10,8 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(AuthorizationUtils_GenericAuthorization.class)
-public abstract class MixinAuthorizationUtils_GenericAuthorization {
+@Mixin(AuthorizationUtils$GenericAuthorization.class)
+public abstract class MixinAuthorizationUtilsGenericAuthorization {
     @Shadow
     public abstract void addParameter(String name, String value);
 
