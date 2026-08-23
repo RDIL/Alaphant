e816337fa2ccd3a5668434be3e84526c8785b8f7
Reece Dunham <me@rdil.rocks>
2026-08-23 13:55:21 -0400

    macos stuff

---
+++ src/main/java/alaphant/mixin/remappedReflection/MacOSManagerMixin.java
@@ -0,0 +1,47 @@
+package alaphant.mixin.remappedReflection;
+
+import com.charlesproxy.macos.MacOSImpl;
+import com.charlesproxy.macos.MacOSManager;
+import com.charlesproxy.macos.gui.MacOSGUIImpl;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+@Mixin(MacOSManager.class)
+public abstract class MacOSManagerMixin {
+    @ModifyArg(
+            method = "isMacOS",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Ljava/lang/Class;forName(Ljava/lang/String;)Ljava/lang/Class;"
+            ),
+            index = 0
+    )
+    private static String alaphant$remapAvailabilityProbe(final String official) {
+        return alaphant$toNamedClass(official);
+    }
+
+    @ModifyArg(
+            method = "getMacOSImpl",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Ljava/lang/Class;forName(Ljava/lang/String;)Ljava/lang/Class;"
+            ),
+            index = 0
+    )
+    private static String alaphant$remapImplementationLookup(final String official) {
+        return alaphant$toNamedClass(official);
+    }
+
+    @Unique
+    private static String alaphant$toNamedClass(final String official) {
+        if ("com.charlesproxy.macos.vQtF".equals(official)) {
+            return MacOSImpl.class.getName();
+        }
+        if ("com.charlesproxy.macos.MkAr.kmft".equals(official)) {
+            return MacOSGUIImpl.class.getName();
+        }
+        return official;
+    }
+}
