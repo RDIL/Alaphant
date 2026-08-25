540e9be616f90cad41a8a2c84785371cc2662625
Reece Dunham <me@rdil.rocks>
2026-08-24 22:11:45 -0400

    another mapping pass

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,6 +11,11 @@ dependencyResolutionManagement {
         maven("https://repo.spongepowered.org/repository/maven-public") { name = "spongepowered" }
         maven("https://maven.fabricmc.net") { name = "fabricmc" }
         maven("https://maven.quiltmc.org/repository/release") { name = "quiltmc" }
+        // Enigma is taken from a fork built by JitPack.
+        maven("https://jitpack.io") {
+            name = "jitpack"
+            content { includeGroupByRegex("com\\.github\\..*") }
+        }
     }
 }
 
